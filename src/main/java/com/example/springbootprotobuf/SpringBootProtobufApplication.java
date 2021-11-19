de9e2cbf645552030602353a83d47bcf3e5281d1
phil.carter <phil.carter@perficient.com>
2021-11-19 08:07:17 -0600

    Initial commit

---
+++ src/main/java/com/example/springbootprotobuf/SpringBootProtobufApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootprotobuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProtobufApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProtobufApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootprotobuf/SpringBootProtobufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootprotobuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProtobufApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
